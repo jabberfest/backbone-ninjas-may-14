fe51f9a0175b36d0e9e82c5ace1b475c072f94a0
Fernando Arias <fernando@jabberfest.com>
2013-05-14 00:17:45 -0700

    Initial Commit, Added Backbone, Marionette, Underscore, and handblebars_assets gem

---
+++ app/controllers/demo_controller.rb
@@ -0,0 +1,9 @@
+class DemoController < ApplicationController
+
+	def index
+		respond_to do |format|
+			format.html
+		end
+	end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BackboneMarionetteDemo::Application.config.secret_token = 'cfc198c61f03862e1e1c2a688b181d2e67754ca09eefb2f937142e8ef06ed04acea6a1df3ab6458108af6a1d9e22001adc43697c9d2d28793bb5ac4f38bbb634'
